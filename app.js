0f56a4bbcafa12daa45ea5947fc22a9c77a643ec
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-01 03:00:40 +0530

    done with scripting part

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var btn = document.querySelector("#btn");
 var txtIn = document.querySelector("#txtInput");
-var choice = document.querySelector("#opt");
+var opt = document.querySelector("#opt");
+var serverURL;
+function translateURL(text){
+	return serverURL + "?" + "text= " + text +" "; 
+}
 
-btn.addEventListener("click",function btnHandler(){
-	console.log(choice.options[choice.selectedIndex].value);
-})
+function errorHandler(error){
+	console.log("error occured "+error);
+	alert("Server is down! Please try after some time.");
+}
+
+function btnHandler(){
+	var choice = opt.options[opt.selectedIndex].value;
+	if(choice=="EtoM"){
+		serverURL = "https://api.funtranslations.com/translate/morse.json";
+	}
+	else{
+		serverURL = "https://api.funtranslations.com/translate/morse2english.json"
+	}
+
+	fetch(translateURL(txtIn.value)).then(response => response.json()).then(json=>{
+	divOut.value=json.contents.translated;
+	}).catch(errorHandler);
+}
+
+btn.addEventListener("click",btnHandler);
